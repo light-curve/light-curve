217db12b56ec12f682d45dd35be5d438ab4cd360
Konstantin Malanchev <hombit@gmail.com>
2019-08-07 20:58:27 +0300

    Refactor fit module and remove Fitter trait

--- light-curve-feature/src/fit.rs
+++ light-curve-feature/src/fit.rs
@@ -1,41 +1,29 @@
-use std::collections::BTreeMap;
-
 use conv::ConvUtil;
 
 use crate::float_trait::Float;
 
-pub struct FitResult<T> {
-    pub values: BTreeMap<&'static str, T>,
-    pub cov: BTreeMap<&'static str, BTreeMap<&'static str, T>>,
+pub fn fit_straight_line<T: Float>(
+    x: &[T],
+    y: &[T],
+    err2: Option<&[T]>,
+) -> StraightLineFitterResult<T> {
+    let fitter: Box<dyn StraightLineFitterTrait<_>> = match err2 {
+        Some(err2) => Box::new(NoisyStraightLineFitter::new(x, y, err2)),
+        None => Box::new(StraightLineFitter::new(x, y)),
+    };
+    fitter.fit()
+}
+
+pub struct StraightLineFitterResult<T> {
+    pub slope: T,
+    pub slope_sigma2: T,
+    pub intercept: T,
+    pub intercept_sigma2: T,
+    pub cov: T,
     pub reduced_chi2: T,
 }
 
-pub trait Fitter<T> {
-    fn fit(&self) -> FitResult<T>;
-}
-
-macro_rules! map {
-    {$($key:expr => $value:expr),+ $(,)?} => {
-        {
-            let mut m = BTreeMap::new();
-            $(
-                m.insert($key, $value);
-            )+
-            m
-        }
-    }
-}
-
-pub struct StraightLineFitResult<T> {
-    slope: T,
-    slope_sigma2: T,
-    intercept: T,
-    intercept_sigma2: T,
-    cov: T,
-    reduced_chi2: T,
-}
-
-pub struct StraightLineFirstCoeffs<T: Float> {
+struct StraightLineFirstCoeffs<T: Float> {
     ss: T,
     sx: T,
     sy: T,
@@ -54,7 +42,7 @@ where
     }
 }
 
-pub struct StraightLineSecondCoeffs<T: Float> {
+struct StraightLineSecondCoeffs<T: Float> {
     st2: T,
     b_st2: T,
 }
@@ -71,14 +59,12 @@ where
     }
 }
 
-pub trait StraightLineFitterTrait<T: Float>: Fitter<T> {
+trait StraightLineFitterTrait<T: Float> {
     fn first_coeffs(&self) -> StraightLineFirstCoeffs<T>;
     fn second_coeffs(&self, sxoss: T) -> StraightLineSecondCoeffs<T>;
-    fn set_errors(&self, result: &mut StraightLineFitResult<T>);
-}
+    fn set_errors(&self, result: &mut StraightLineFitterResult<T>);
 
-impl<T: Float, F: StraightLineFitterTrait<T>> Fitter<T> for F {
-    fn fit(&self) -> FitResult<T> {
+    fn fit(&self) -> StraightLineFitterResult<T> {
         let c1 = self.first_coeffs();
         let sxoss = c1.sx / c1.ss;
         let c2 = self.second_coeffs(sxoss);
@@ -87,7 +73,7 @@ impl<T: Float, F: StraightLineFitterTrait<T>> Fitter<T> for F {
         let sig2a = (T::one() + c1.sx.powi(2) / (c1.ss * c2.st2)) / c1.ss;
         let sig2b = T::recip(c2.st2);
         let cov = -c1.sx / (c1.ss * c2.st2);
-        let mut result = StraightLineFitResult {
+        let mut result = StraightLineFitterResult {
             slope: b,
             slope_sigma2: sig2b,
             intercept: a,
@@ -96,18 +82,11 @@ impl<T: Float, F: StraightLineFitterTrait<T>> Fitter<T> for F {
             reduced_chi2: T::zero(),
         };
         self.set_errors(&mut result);
-        FitResult {
-            values: map! {"slope" => result.slope, "intercept" => result.intercept},
-            cov: map! {
-                "slope" => map!{"slope" => result.slope_sigma2, "intercept" => result.cov},
-                "intercept" => map!{"intercept" => result.intercept_sigma2, "slope" => result.cov},
-            },
-            reduced_chi2: result.reduced_chi2,
-        }
+        result
     }
 }
 
-pub struct StraightLineFitter<'a, 'b, T: Float> {
+struct StraightLineFitter<'a, 'b, T: Float> {
     x: &'a [T],
     y: &'b [T],
 }
@@ -146,7 +125,7 @@ where
         coeffs
     }
 
-    fn set_errors(&self, result: &mut StraightLineFitResult<T>) {
+    fn set_errors(&self, result: &mut StraightLineFitterResult<T>) {
         for (&x, &y) in self.x.iter().zip(self.y.iter()) {
             result.reduced_chi2 += (y - result.intercept - result.slope * x).powi(2);
         }
@@ -157,13 +136,13 @@ where
     }
 }
 
-pub struct StraightLineNoisyFitter<'a, 'b, 'c, T: Float> {
+struct NoisyStraightLineFitter<'a, 'b, 'c, T: Float> {
     x: &'a [T],
     y: &'b [T],
     err2: &'c [T],
 }
 
-impl<'a, 'b, 'c, T> StraightLineNoisyFitter<'a, 'b, 'c, T>
+impl<'a, 'b, 'c, T> NoisyStraightLineFitter<'a, 'b, 'c, T>
 where
     T: Float,
 {
@@ -174,7 +153,7 @@ where
     }
 }
 
-impl<'a, 'b, 'c, T> StraightLineFitterTrait<T> for StraightLineNoisyFitter<'a, 'b, 'c, T>
+impl<'a, 'b, 'c, T> StraightLineFitterTrait<T> for NoisyStraightLineFitter<'a, 'b, 'c, T>
 where
     T: Float,
 {
@@ -200,7 +179,7 @@ where
         coeffs
     }
 
-    fn set_errors(&self, result: &mut StraightLineFitResult<T>) {
+    fn set_errors(&self, result: &mut StraightLineFitterResult<T>) {
         for ((&x, &y), &err2) in self.x.iter().zip(self.y.iter()).zip(self.err2.iter()) {
             result.reduced_chi2 += (y - result.intercept - result.slope * x).powi(2) / err2;
         }
@@ -217,38 +196,29 @@ mod tests {
     fn straight_line_fitter() {
         let x = [0.5, 1.5, 2.5, 5.0, 7.0, 16.0];
         let y = [-1.0, 3.0, 2.0, 6.0, 10.0, 25.0];
-        let names = ["intercept", "slope"];
         // scipy.optimize.curve_fit(absolute_sigma=False)
         let actual_values = [-1.3303457, 1.63021767];
-        let actual_cov = [0.44109184, -0.04231878, -0.04231878, 0.0078127];
+        let actual_cov = [0.44109184, -0.04231878, 0.0078127];
         let result = StraightLineFitter::new(&x[..], &y[..]).fit();
-        let result_ref = &result;
-        let desired_values: Vec<_> = names.iter().map(|&name| result.values[name]).collect();
-        let desired_cov: Vec<_> = names
-            .iter()
-            .flat_map(|&name1| names.iter().map(move |&name2| result_ref.cov[name1][name2]))
-            .collect();
+        let desired_values = [result.intercept, result.slope];
+        let desired_cov = [result.intercept_sigma2, result.cov, result.slope_sigma2];
         all_close(&actual_values[..], &desired_values[..], 1e-6);
         all_close(&actual_cov[..], &desired_cov[..], 1e-6);
     }
 
     #[test]
-    fn straight_line_noisy_fitter() {
+    fn noisy_straight_line_fitter() {
         let x = [0.5, 1.5, 2.5, 5.0, 7.0, 16.0];
         let y = [-1.0, 3.0, 2.0, 6.0, 10.0, 25.0];
         let err = [0.5, 1.0, 0.3, 0.1, 0.9, 2.5];
         let names = ["intercept", "slope"];
         // scipy.optimize.curve_fit(absolute_sigma=True)
         let actual_values = [-1.77189545, 1.60504579];
-        let actual_cov = [0.20954795, -0.03651815, -0.03651815, 0.00868733];
+        let actual_cov = [0.20954795, -0.03651815, 0.00868733];
         let actual_reduced_chi2 = [1.8057513419557492];
-        let result = StraightLineNoisyFitter::new(&x[..], &y[..], &err[..]).fit();
-        let result_ref = &result;
-        let desired_values: Vec<_> = names.iter().map(|&name| result.values[name]).collect();
-        let desired_cov: Vec<_> = names
-            .iter()
-            .flat_map(|&name1| names.iter().map(move |&name2| result_ref.cov[name1][name2]))
-            .collect();
+        let result = NoisyStraightLineFitter::new(&x[..], &y[..], &err[..]).fit();
+        let desired_values = [result.intercept, result.slope];
+        let desired_cov = [result.intercept_sigma2, result.cov, result.slope_sigma2];
         all_close(&actual_values[..], &desired_values[..], 1e-6);
         all_close(&actual_cov[..], &desired_cov[..], 1e-6);
         all_close(&actual_reduced_chi2[..], &[result.reduced_chi2], 1e-6);
