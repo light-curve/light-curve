ddd5c96c0f5df5594555420c8e2599f7773e792b
stlavrukhina <lavrukhina.ad@gmail.com>
2020-10-31 18:35:00 +0300

    Add mean feature
    Change tests names

---
+++ python/light_curve_pure/features/mean.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from ._base import BaseFeature
+
+class Mean(BaseFeature):
+    def __call__(self, t, m, sigma=None, sorted=None, fill_value=None):
+        return np.mean(m)
+
+
+__all__ = ('Mean',)
