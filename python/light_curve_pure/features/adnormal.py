3ee61a7aa4ded743bf6bbe92e2be9dcf76e919f0
stlavrukhina <lavrukhina.ad@gmail.com>
2020-11-20 17:53:39 +0300

    adnormal.py

--- python/light_curve_pure/features/adnormal.py
+++ python/light_curve_pure/features/adnormal.py
@@ -8,7 +8,6 @@ class AndersonDarlingNormal(BaseFeature):
     def __call__(self, t, m, sigma=None, sorted=None, fill_value=None):
         n = len(m)
         return anderson(m).statistic * (1 + 4 / n - 25 / n ** 2)
-    
+
 
 __all__ = ("AndersonDarlingNormal",)
-
