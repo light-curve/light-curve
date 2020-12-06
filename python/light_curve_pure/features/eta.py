833707530f26bbace83044f003cd357efb92c6d9
stlavrukhina <lavrukhina.ad@gmail.com>
2020-12-07 00:34:48 +0300

    EtaE, Eta fix

--- python/light_curve_pure/features/eta.py
+++ python/light_curve_pure/features/eta.py
@@ -7,7 +7,7 @@ class Eta(BaseFeature):
     def __call__(self, t, m, sigma=None, sorted=None, fill_value=None):
         n = len(m)
         m_std = np.var(m, ddof=1)
-        m_sum = np.sum(np.subtract(m[1:], m[:-1]) ** 2)
+        m_sum = np.sum((m[1:] - m[:-1]) ** 2)
         return m_sum / ((n - 1) * m_std)
 
 
--- python/light_curve_pure/features/etae.py
+++ python/light_curve_pure/features/etae.py
@@ -7,7 +7,7 @@ class EtaE(BaseFeature):
     def __call__(self, t, m, sigma=None, sorted=None, fill_value=None):
         n = len(m)
         m_std = np.var(m, ddof=1)
-        m_sum = np.sum(((np.subtract(m[1:], m[:-1]) / np.subtract(t[1:], t[:-1])) ** 2))
+        m_sum = np.sum(((m[1:] - m[:-1]) / (t[1:] - t[:-1])) ** 2)
         return m_sum * (t[n - 1] - t[0]) ** 2 / ((n - 1) ** 3 * m_std)
 
 
