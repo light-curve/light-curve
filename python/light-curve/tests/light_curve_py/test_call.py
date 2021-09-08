b0b142bffc867a3571e294a87f08eea657fa115d
stlavrukhina <lavrukhina.ad@gmail.com>
2021-09-08 15:41:38 +0300

    fill_value changed, tests for 'fill_value' and 'sorted' added

--- python/light-curve/tests/light_curve_py/test_call.py
+++ python/light-curve/tests/light_curve_py/test_call.py
@@ -1,6 +1,6 @@
+import numpy as np
 from numpy.testing import assert_allclose
-
-from light_curve.light_curve_py import MaximumSlope, Kurtosis
+from light_curve.light_curve_py import MaximumSlope
 
 
 def test_normalize():
@@ -9,4 +9,20 @@ def test_normalize():
     feature = MaximumSlope()
     actual = feature(t, m, None, sorted=False)
     desired = 11.0
+    assert_allclose(actual, desired)
+
+
+def test_fill_zero_division():
+    t = [1, 1, 3, 4]
+    feature = MaximumSlope()
+    actual = feature(t, t, None, sorted=False, fill_value=[1.0])
+    desired = 1.0
+    assert_allclose(actual, desired)
+
+
+def test_fill_nan_values():
+    t = [np.nan, 1, 3, 4]
+    feature = MaximumSlope()
+    actual = feature(t, t, None, sorted=False, fill_value=[1.0])
+    desired = 1.0
     assert_allclose(actual, desired)
