88c07cb423733cfdf0e132a8dcd629b6b195ad9a
Konstantin Malanchev <hombit@gmail.com>
2021-10-14 12:05:59 +0200

    Add __call__ vs many test

--- light-curve-python/tests/test_feature.py
+++ light-curve-python/tests/test_feature.py
@@ -2,7 +2,7 @@ import inspect
 
 import numpy as np
 import pytest
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_array_equal
 
 import light_curve as lc
 
@@ -30,6 +30,16 @@ non_param_feature_classes = frozenset(_feature_classes(lc, True))
 all_feature_classes = frozenset(_feature_classes(lc, False))
 
 
+def gen_lc(n, rng=None):
+    rng = np.random.default_rng(rng)
+
+    t = np.sort(rng.normal(0, 1, n))
+    m = t.copy()
+    sigma = np.full_like(t, 0.1)
+
+    return t, m, sigma
+
+
 @pytest.mark.parametrize("cls", non_param_feature_classes)
 def test_negative_strides(cls):
     t = np.linspace(1, 0, 20)[::-2]
@@ -44,15 +54,26 @@ def test_float32_vs_float64(cls):
     rng = np.random.default_rng(0)
     n = 128
 
+    t, m, sigma = gen_lc(n, rng=rng)
     obj = cls()
-
-    t = np.sort(rng.normal(0, 1, n))
-    m = t.copy()
-    sigma = np.full_like(t, 0.1)
 
     results = [obj(t.astype(dtype), m.astype(dtype), sigma.astype(dtype), sorted=True)
                for dtype in [np.float32, np.float64]]
     assert_allclose(*results, rtol=1e-5, atol=1e-5)
+
+
+@pytest.mark.parametrize("cls", non_param_feature_classes)
+def test_many_vs_call(cls):
+    rng = np.random.default_rng(0)
+    n_obs = 128
+    n_lc = 128
+
+    lcs = [gen_lc(n_obs, rng=rng) for _ in range(n_lc)]
+    obj = cls()
+
+    call = np.stack([obj(*lc, sorted=True) for lc in lcs])
+    many = obj.many(lcs, sorted=True, n_jobs=2)
+    assert_array_equal(many, call)
 
 
 @pytest.mark.parametrize("cls", all_feature_classes)
