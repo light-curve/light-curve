5513e3b00257e1c224419eeeb37e71ab0f378f2c
stlavrukhina <lavrukhina.ad@gmail.com>
2021-11-10 17:19:29 +0300

    LinearTrend fix

--- python/light-curve/light_curve/light_curve_py/features/lintrend.py
+++ python/light-curve/light_curve/light_curve_py/features/lintrend.py
@@ -8,14 +8,11 @@ class LinearTrend(BaseFeature):
     def _eval(self, t, m, sigma=None):
         n = len(t)
 
-        if n == 2:
-            return (m[1] - m[0]) / (t[1] - t[0]), 0, 0
-
         slope, chi2 = least_squares(t, m, None)
 
         red_chi2 = chi2 / (n - 2)
         sxx = np.var(t, ddof=n - 1)
-        return np.array([slope, np.sqrt(red_chi2 / sxx), red_chi2])
+        return np.array([slope, np.sqrt(red_chi2 / sxx), np.sqrt(red_chi2)])
 
     @property
     def size(self):
