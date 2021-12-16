1e74064a6e191f916d2949ae4d7fb1436afc8faa
Konstantin Malanchev <hombit@gmail.com>
2021-12-16 17:17:11 -0600

    Delete python directory
    Python packages development is moved to a new repo

--- python/light-curve/light_curve/__init__.py
+++
@@ -1,9 +0,0 @@
-from .light_curve_py import *
-
-# Hide Python features with Rust equivalents
-from .light_curve_ext import *
-
-# Hide Rust Extractor with universal Python Extractor
-from .light_curve_py import Extractor
-
-from .light_curve_ext import __version__
--- python/light-curve/light_curve/light_curve_ext.py
+++
@@ -1,1 +0,0 @@
-from .light_curve import *
